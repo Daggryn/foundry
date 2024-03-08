522114f5afa0cc65b1c39206b0699f8083e09ec2
daggryn <joshmaynard28@gmail.com>
2024-03-08 17:42:08 +0100

    plan

---
+++ macros/bardic-inspiration.js
@@ -0,0 +1,11 @@
+/*
+Targetted token's actor recieves a dice roll they can use for any ability check, saving throw, or attack roll.
+Whenever the target has such roll, they are asked if they want to add the dice to their roll.
+When the roll is used, the dice is consumed and the effect ends.
+*/
+
+async function main () {
+  //
+}
+
+main()
